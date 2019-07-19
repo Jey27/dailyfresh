cf8fb034205ee4e83ec73abce03630d70e95a6da
Jay27 <jiege2727@163.com>
2019-07-19 10:31:19 +0800

    商品详情页及用户中心历史浏览记录

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,7 +1,8 @@
 
 from django.conf.urls import url
-from goods.views import IndexView
+from goods.views import IndexView,DetailView
 
 urlpatterns = [
     url(r'^$',IndexView.as_view(),name='index'),
+    url(r'^goods/(?P<goods_id>\d+)$',DetailView.as_view(),name = 'detail'),
 ]
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.urlresolvers import reverse
 from django.views.generic import View
-from goods.models import GoodsType,IndexGoodsBanner,IndexPromotionBanner,IndexTypeGoodsBanner
+from goods.models import GoodsType,IndexGoodsBanner,IndexPromotionBanner,IndexTypeGoodsBanner,GoodsSKU
+from order.models import OrderGoods
 from django_redis import get_redis_connection
 # Create your views here.
 
@@ -41,3 +43,48 @@ class IndexView(View):
         }
 
         return render(request,'index.html',context)
+
+class DetailView(View):
+    '''商品详情页'''
+    def get(self,request,goods_id):
+        # 获取商品信息
+        try:
+            sku = GoodsSKU.objects.get(id=goods_id)
+        except GoodsSKU.DoesNotExist:
+            # 商品不存在
+            return redirect(reverse('goods:index'))
+        # 获取商品的分类信息
+        types = GoodsType.objects.all()
+        # 获取商品的评论信息
+        sku_orders = OrderGoods.objects.filter(sku=sku).exclude(comment='')
+        # 获取新品信息
+        new_skus = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')[:2]
+        # 获取同一个spu的商品信息
+        same_skus = GoodsSKU.objects.filter(goods=sku.goods).exclude(id=goods_id)
+        # 获取购物车商品数目
+        cart_count = 0
+        user = request.user
+        if user.is_authenticated():
+            # 用户已登录
+            conn = get_redis_connection('default')
+            cart_key = 'cart_%d' % user.id
+            cart_count = conn.hlen(cart_key)
+            # 添加用户历史记录
+            conn = get_redis_connection('default')
+            history_key = 'history_%d'%user.id
+            # 移除已存在的历史记录
+            conn.lrem(history_key,0,goods_id)
+            # 吧goodsid左侧插入
+            conn.lpush(history_key,goods_id)
+            # 只保存用户最新浏览的五条信息
+            conn.ltrim(history_key,0,4)
+        # 组织上下文
+        context = {
+            'sku':sku,
+            'types':types,
+            'sku_orders':sku_orders,
+            'new_skus':new_skus,
+            'cart_count':cart_count,
+            'same_skus':same_skus,
+        }
+        return render(request,'detail.html',context)
