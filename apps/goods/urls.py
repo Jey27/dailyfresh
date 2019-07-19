7257b6db01e71e68587dbd4eea81b64d9bd30f5d
Jay27 <jiege2727@163.com>
2019-07-19 15:04:14 +0800

    商品列表页的完成

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,8 +1,9 @@
 
 from django.conf.urls import url
-from goods.views import IndexView,DetailView
+from goods.views import IndexView,DetailView,ListView
 
 urlpatterns = [
     url(r'^$',IndexView.as_view(),name='index'),
     url(r'^goods/(?P<goods_id>\d+)$',DetailView.as_view(),name = 'detail'),
+    url(r'list/(?P<type_id>\d+)/(?P<page>\d+)$',ListView.as_view(),name = 'list')
 ]
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.core.urlresolvers import reverse
 from django.views.generic import View
+from django.core.paginator import Paginator
 from goods.models import GoodsType,IndexGoodsBanner,IndexPromotionBanner,IndexTypeGoodsBanner,GoodsSKU
 from order.models import OrderGoods
 from django_redis import get_redis_connection
@@ -88,3 +89,75 @@ class DetailView(View):
             'same_skus':same_skus,
         }
         return render(request,'detail.html',context)
+
+# list/种类id/页码?sort=排序方式
+class ListView(View):
+    '''列表页'''
+    def get(self,request,type_id,page):
+        '''显示列表页'''
+        # 获取种类信息
+        try:
+            type = GoodsType.objects.get(id=type_id)
+        except GoodsType.DoesNotExist:
+            return redirect(reverse('goods:index'))
+        # 获取商品的分类信息
+        types = GoodsType.objects.all()
+        # 获取排序方式 获取分类商品的信息
+        # sort=default 按照默认进行排序
+        # sort=price 按照价格进行排序
+        # sort=hot 按照销量进行排序
+        sort = request.GET.get('sort')
+
+        if sort=='price':
+            skus = GoodsSKU.objects.filter(type=type).order_by('price')
+        elif sort=='hot':
+            skus = GoodsSKU.objects.filter(type=type).order_by('-sales')
+        else:
+            sort='default'
+            skus = GoodsSKU.objects.filter(type=type).order_by('id')
+        # 对数据进行分页
+        paginator = Paginator(skus,1)
+        try:
+            page=int(page)
+        except Exception as e:
+            page = 1
+        if page>paginator.num_pages:
+            page=paginator.num_pages
+        # 获取第page页的内容
+        skus_page= paginator.page(page)
+        # 进行页码控制，页面上最多显示5个页码
+        # 1.总数小于5页，显示所有页
+        # 2.当前是后三页时，显示后5页
+        # 3.当前为前三页时，显示1-5页
+        # 4. 其他情况时，显示当前页的前两页和后两页
+        page_num = paginator.num_pages
+        if page_num<=5:
+            pages = range(1,page_num+1)
+        elif page<=3:
+            pages = range(1,6)
+        elif page_num-page<=2:
+            pages = range(page_num-4,page_num+1)
+        else:
+            pages = range(range-2,range+3)
+        # 获取新品信息
+        new_skus = GoodsSKU.objects.filter(type=type).order_by('-create_time')[:2]
+        # 获取购物车商品数目
+        cart_count = 0
+        user = request.user
+        if user.is_authenticated():
+            # 用户已登录
+            conn = get_redis_connection('default')
+            cart_key = 'cart_%d' % user.id
+            cart_count = conn.hlen(cart_key)
+        # 组织模板上下文
+        context={
+            'type':type,
+            'types':types,
+            'skus_page':skus_page,
+            'new_skus':new_skus,
+            'cart_count':cart_count,
+            'sort':sort,
+            'pages':pages
+        }
+
+        return render(request,'list.html',context)
